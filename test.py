ad870701152a54fd3eeebf35d5b7aa79d815c1a4
Ibrahimnalmanaseer <Ibrahimnalmanaseer@gmail.com>
2024-04-17 14:29:08 +0300

    version 2 : without genesys api

--- test.py
+++ test.py
@@ -1,18 +1,13 @@
-recording_ids = [
-    '580704103_580704103_3001550406F0240401_01April2024_220436',
-    '580842427_580842427_3001550519F0240401_01April2024_221246',
-    '581333626_581333626_3001550311F0240401_01April2024_215933',
-    '590691174_590691174_3001550281F0240401_01April2024_215802',
-    '590691174_590691174_3001550466F0240401_01April2024_220741',
-    '591602222_591602222_3001550590F0240401_01April2024_221607',
-    '592285555_592285555_3001550309F0240401_01April2024_220048',
-    '592877888_592877888_3001550340F0240401_01April2024_220229',
-    '593180512_593180512_3001550231F0240401_01April2024_215508',
-    '593825453_593825453_3001550654F0240401_01April2024_221750'
-]
-
-# Add single quotes around each RecordingId value
-quoted_recording_ids = [f"'{recording_id}'" for recording_id in recording_ids]
 
 
-print(quoted_recording_ids)
+
+
+my_dict = {'record1': 1, 'record2': 1, 'record3': 1, 'record4': 0}
+
+keys_with_value_0 = []
+
+for key, value in my_dict.items():
+    if value == 0:
+        keys_with_value_0.append(key)
+
+print(keys_with_value_0)
