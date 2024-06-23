f56ea77521d2c3fc90284e63cb0ff081dded9048
Ibrahim <Ibrahimnalmanaseer@gmail.com>
2024-06-23 18:13:22 +0300

    version 4: add dirctory for each wav, dtabase connection

--- mainclass.py
+++ mainclass.py
@@ -2,11 +2,21 @@ import requests
 import os
 import logging
 import json
+import wave
+import time
+import shutil
+import contextlib
+import datetime
 
+import pyodbc
 import time
 class MainAPI:
 
-    wav_files= {}
+
+
+
+
+
     token_ourapi = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjdXN0X25hbWUiOiJBYnUgWWFtYW4iLCJjdXN0X2lkIjoyfQ.-3LYg1zcUwAG_Z5omho420alNGIC-nKmSrA7E0wNbMk'
 
     def __init__(self, log_directory='C:\\traces', log_file_name='api_request_log.txt'):
@@ -14,6 +24,25 @@ class MainAPI:
         self.log_file_name = log_file_name
         self.log_file_path = os.path.join(log_directory, log_file_name)
         self.setup_logging()
+        self.wav_files = []
+        self.wav_dur_modified_date={}
+
+
+    def load_wav_files(self):
+
+        try:
+            with open ('C:\\traces\wav_files_dictionary.json',"r") as a:
+                json_data = a.read()
+                loaded_dict = json.loads(json_data)
+                return loaded_dict
+        except :
+            return {}
+
+    def save_wav_files(self):
+
+        with open('C:\\traces\wav_files_dictionary.json', "w") as b:
+
+            json.dump(self.wav_files,b)
 
 
     def setup_logging(self):
@@ -37,15 +66,43 @@ class MainAPI:
 
             try:
 
-                self.get_wav_files(folder_path)
-                self.log_message(f"+++++++{folder_path}")
-                for wav,status in self.wav_files.items():
-                    if status==0:
+                self.get_wav_files_path(folder_path)
+
+                if self.wav_files:
+                    for wav in self.wav_files:
 
 
-                        self.send_api_request('http://172.16.54.15:3000/rec_request/', self.token_ourapi, f"{wav}")
-                        self.log_message(f'successful transcription of: {wav}')
-                        self.wav_files[wav]=1
+                            # self.send_api_request('http://172.16.54.15:3000/rec_request/', self.token_ourapi, f"{wav}")
+
+
+
+
+
+
+
+                            self.wav_dur_modified_date[wav] = {
+                                "wav_duration": str(self.get_wav_duration(wav)),
+                                "wav_modified_date": str(self.get_wav_modified_date(wav)),
+                                "wav_file_name": os.path.basename(wav),
+                                "wav_directory": self.move_wav_to_destination(r"C:\\traces",wav,self.get_wav_modified_date(wav))
+                            }
+
+                            self.wav_files=[]
+
+
+                            logging.info(f"dur_modified_date: : {self.wav_dur_modified_date}")
+
+
+
+
+
+                else:
+                    logging.info("no files found in wav_files folder")
+
+
+
+
+
 
 
 
@@ -53,25 +110,25 @@ class MainAPI:
 
             except Exception as e:
 
-                self.log_message_error(f'Errorttttttt: {e}')
-
-            self.log_message(self.wav_files)
-
-            time.sleep(60)
+                self.log_message_error(f'Error ttt: {e}')
 
 
+            logging.info("sleep 120")
+            time.sleep(120)
 
 
 
 
-    def get_wav_files(self,folder_path):
+
+
+    def get_wav_files_path(self,folder_path):
 
         for root, dirs, files in os.walk(folder_path):
             for file in files:
                 if file.endswith(".wav"):
                     full_path = os.path.join(root, file)
                     if full_path not in self.wav_files:
-                        self.wav_files[full_path]=0
+                        self.wav_files.append(full_path)
 
 
 
@@ -89,19 +146,103 @@ class MainAPI:
             response = requests.post(api_url, headers=headers, files={'file': open(file_path, 'rb')})
             if response.status_code == 200:
                 words = [item["word"] for item in response.json()["agent"]]
-
                 words_text = " ".join(words)
-                with open(f'D:\\NCGR_QUICK_WIN\VoiceMail\{file_name_without_extension}.json', 'w', encoding='utf-8') as f:
-                    json.dump(words_text, f, ensure_ascii=False)
+                with open(f'D:\\NCGR_QUICK_WIN\VoiceMail\{file_name_without_extension}.txt', 'w', encoding='utf-8') as f:
+                    # json.dump(words_text, f, ensure_ascii=False)
+                    f.write(words_text)
+
+                with open(f'D:\\Json_Backup\VoiceMail\{file_name_without_extension}.txt', 'w', encoding='utf-8') as f:
+                    f.write(words_text)
+
+
+                self.copy_files("D:\\NCGR_QUICK_WIN\VoiceMail",file_path,file_name_with_extension)
+                self.copy_files("D:\\Json_Backup\VoiceMail", file_path, file_name_with_extension)
+
                 self.log_message("API request successful")
             else:
-                self.log_message_error(f"API request failed with status code: {response.status_code}, response: {response.text}")
-        except Exception as e:
+                # call_duration=self.get_wav_duration(file_path)
+                self.log_message_error(f"API request failed with status code: {response.status_code}, response: {response.text} ,File Name:{file_name_with_extension}, Call length per Sec : ")
+
+        except FileNotFoundError:
+            self.log_message_error(f"File not found: {file_path}")
+
+        except requests.exceptions.RequestException as e:
             self.log_message_error(f"Error sending API request: {e}")
 
+    def get_wav_duration(self,file_path):
+        with contextlib.closing(wave.open(file_path, 'r')) as wf:
+            frames = wf.getnframes()
+            rate = wf.getframerate()
+            duration = int(frames / float(rate))
+
+            return duration
+
+    def get_wav_modified_date(self,file_path):
+
+        modified_date = datetime.datetime.fromtimestamp(os.path.getmtime(file_path))
+
+        return modified_date
+
+    def move_wav_to_destination(self,destination_dir,file_path,modified_date):
+
+        
+
+        year_folder = os.path.join(destination_dir, str(modified_date.year))
+        month_folder = os.path.join(year_folder, str(modified_date.month).zfill(2))
+        day_folder = os.path.join(month_folder, str(modified_date.day).zfill(2))
+
+        for folder in [year_folder, month_folder, day_folder]:
+            if not os.path.exists(folder):
+                os.makedirs(folder)
+
+        # Move file
+        destination_file = os.path.join(day_folder, os.path.basename(file_path))
+        shutil.move(file_path, destination_file)
+        return day_folder
+
+
+
+    def copy_files(self,destination_path,file_path,file_name_with_extension):
+
+
+        shutil.copyfile(file_path, os.path.join(destination_path, file_name_with_extension))
+
+
+
+
+    def database_connect(self,call_info):
+
+        server = 'ccclustervs,1434'
+        database = 'Speechlog_Central'
+        username = 'UCCE_CCS'
+        password = 'BSFipcc_ist01'
+
+        conn_str = f'DRIVER={{ODBC Driver 13 for SQL Server}};SERVER={server};DATABASE={database};UID={username};PWD={password}'
+
+        insert_to_recorded_msgs = """
+        INSERT INTO dbo.RecordedMsgs (CallDate, Duration, DirectoryPath, filename, Extension, CallerID, CalledID, AgentID, TrunkNo, IsHeard, Phone, Nature, calltype, RingCount, DialedNumber,
+                                      CallerName, Backed, BackCDNum, BackupCallNum, Deleted, SysMode, FileFormat, VideoFileName, IsProtected, PrivateCall, IsEvaluated, AddedDate, isAnalysed, IsAnalyzed, IsNonFCRParent, NonFCRCount, holdtime, CallID)
+        SELECT CallDate, Duration, DirectoryPath, filename, Extension, CallerID, CalledID, AgentID, TrunkNo, IsHeard, Phone, Nature, calltype, RingCount, DialedNumber,
+               CallerName, Backed, BackCDNum, BackupCallNum, Deleted, SysMode, FileFormat, VideoFileName, IsProtected, PrivateCall, IsEvaluated, AddedDate, isAnalysed, IsAnalyzed, IsNonFCRParent, NonFCRCount, holdtime, CallID
+        FROM dbo.processedRecords 
+        """
+
+        try:
+            conn = pyodbc.connect(conn_str)
+            cursor = conn.cursor()
+            cursor.execute(insert_to_recorded_msgs)
+            conn.commit()
+
+        except Exception as e:
+           self.log_message_error(f"Error executing {e}")
+           raise
+
+        finally:
+            if conn:
+                conn.close()
 
 if __name__ == '__main__':
 
     api=MainAPI()
 
-    api.main_method("D:\\NCGR_QUICK_WIN\Genesys_Calls")
+    api.main_method(r"C:\Users\Ibrahim\Documents")
--- test.py
+++ test.py
@@ -1,13 +1,55 @@
+import os
+import wave
+import contextlib
+import datetime
+import shutil
+
+# # Sample dictionary
+# my_dict = {"key1": "value1", "key2": "value2"}
+#
+# # Convert dictionary to JSON string
+# json_str = json.dumps(my_dict)
+#
+# # Write JSON string to log file
+# with open('log_file.txt', 'w') as log_file:
+#     log_file.write(json_str)
+#
+# # Read JSON string from log file and load it
+# with open('log_file.txt', 'r') as log_file:
+#     json_data = log_file.read()
+#     loaded_dict = json.loads(json_data)
+#
+# print(loaded_dict)
+
+modified_date = datetime.datetime.fromtimestamp(os.path.getmtime(r"C:\Users\Ibrahim\Downloads\speechlog-web-app\speechlog-web-app\wwwroot\Uploads\SessionAttachments\2020\7\23\1595505668922.wav"))
+
+with contextlib.closing(wave.open(r"C:\Users\Ibrahim\Downloads\speechlog-web-app\speechlog-web-app\wwwroot\Uploads\SessionAttachments\2020\7\23\1595505668922.wav", 'r')) as wf:
+    frames = wf.getnframes()
+    rate = wf.getframerate()
+    duration = int(frames / float(rate))
+
+
+destination_dir =r'C:\\traces\07'
+
+source_file = r'C:\traces\Emerging trends and topics (3).png'
+
+year_folder = os.path.join(destination_dir, str(modified_date.year))
+month_folder = os.path.join(year_folder, str(modified_date.month).zfill(2))
+day_folder = os.path.join(month_folder, str(modified_date.day).zfill(2))
+
+
+for folder in [year_folder, month_folder, day_folder]:
+    if not os.path.exists(folder):
+        os.makedirs(folder)
 
 
 
+# Move file
+destination_file = os.path.join(day_folder, os.path.basename(source_file))
+shutil.move(source_file, destination_file)
 
-my_dict = {'record1': 1, 'record2': 1, 'record3': 1, 'record4': 0}
+# shutil.move(r"C:\traces\Emerging trends and topics.png", r"C:\\traces\05")
 
-keys_with_value_0 = []
+print (modified_date)
 
-for key, value in my_dict.items():
-    if value == 0:
-        keys_with_value_0.append(key)
-
-print(keys_with_value_0)
+print(duration)
